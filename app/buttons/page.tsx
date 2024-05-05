27035882ebdcde368aa7ee517a870aa14f067fb2
Griego86 <dimi1086@hotmail.com>
2024-05-05 15:09:16 -0700

    Added indigo-colored buttons

--- app/buttons/page.tsx
+++ app/buttons/page.tsx
@@ -24,6 +24,12 @@ const ButtonsPage = () => {
       <Button variant="dangerOutline">
         Danger Outline
       </Button>
+      <Button variant="super">
+        Super
+      </Button>
+      <Button variant="superOutline">
+        Super Outline
+      </Button>
     </div>
   )
 }
