73b81c838bdba84ae624219a0a12af7c65fce4fd
Griego86 <dimi1086@hotmail.com>
2024-05-06 09:31:23 -0700

    Added page for learn route & layout

---
+++ app/(main)/layout.tsx
@@ -0,0 +1,14 @@
+type Props = {
+  children: React.ReactNode
+}
+
+
+const MainLayout = ({children}: Props) => {
+  return (
+    <div>
+      {children}
+    </div>
+  )
+}
+
+export default MainLayout
