d4f6382fea33356376ef8cda4efeac46a6c55401
Griego86 <dimi1086@hotmail.com>
2024-05-06 09:25:04 -0700

    Finished footer landing page

--- app/(marketing)/footer.tsx
+++ app/(marketing)/footer.tsx
@@ -1,7 +1,51 @@
+import { Button } from "@/components/ui/button"
+import Image from "next/image"
+
 export const Footer = () => {
   return(
     <footer className="hidden lg:block h-20 w-full border-t-2 border-slate-200 p-2">
-      Footer
+      <div className="max-w-screen-lg mx-auto flex items-center justify-evenly h-full">
+        <Button size='lg' variant='ghost' className="w-full">
+          <Image 
+            src='/spain.png'
+            alt="Spain"
+            height={32}
+            width={40}
+            className="mr-4 rounded-md"
+          />
+          Spanish
+        </Button>
+        <Button size='lg' variant='ghost' className="w-full">
+          <Image 
+            src='/france.png'
+            alt="France"
+            height={32}
+            width={40}
+            className="mr-4 rounded-md"
+          />
+          French
+        </Button>
+        <Button size='lg' variant='ghost' className="w-full">
+          <Image 
+            src='/italy.png'
+            alt="Italy"
+            height={32}
+            width={40}
+            className="mr-4 rounded-md"
+          />
+          Italian
+        </Button>
+        <Button size='lg' variant='ghost' className="w-full">
+          <Image 
+            src='/catalonia.png'
+            alt="Catalonia"
+            height={32}
+            width={40}
+            className="mr-4 rounded-md"
+          />
+          Catalan
+        </Button>
+      </div>
     </footer>
   )
 }
