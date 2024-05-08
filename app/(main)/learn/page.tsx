e756335e8913450e448e458030b4212149ad423b
Griego86 <dimi1086@hotmail.com>
2024-05-08 10:46:19 -0700

    Added left arrow

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,12 +1,13 @@
 import { FeedWrapper } from '@/components/feed-wrapper'
 import { StickyWrapper } from '@/components/sticky-wrapper'
 import React from 'react'
+import { Header } from './header'
 
 const LearnPage = () => {
   return (
     <div className='flex gap-[48px] px-6'>
       <FeedWrapper>
-        My feed
+        <Header title="Spanish" />
       </FeedWrapper>
       <StickyWrapper>
         My sticky sidebar
