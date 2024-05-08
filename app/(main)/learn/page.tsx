663c3f635cd49c420c81a50ba377900e14d30995
Griego86 <dimi1086@hotmail.com>
2024-05-08 10:59:27 -0700

    Added progress bar

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -2,6 +2,7 @@ import { FeedWrapper } from '@/components/feed-wrapper'
 import { StickyWrapper } from '@/components/sticky-wrapper'
 import React from 'react'
 import { Header } from './header'
+import { UserProgress } from '@/components/user-progress'
 
 const LearnPage = () => {
   return (
@@ -10,7 +11,12 @@ const LearnPage = () => {
         <Header title="Spanish" />
       </FeedWrapper>
       <StickyWrapper>
-        My sticky sidebar
+        <UserProgress
+          activeCourse={{}}
+          hearts={5}
+          points={100}
+          hasActiveSubscription={false}
+        />
       </StickyWrapper>
     </div>
   )
---
+++ components/user-progress.tsx
@@ -0,0 +1,14 @@
+type Props = {
+  activeCourse: any
+  hearts: number
+  points: number
+  hasActiveSubscription: Boolean
+}
+
+export const UserProgress = ({}: Props) => {
+  return(
+    <div>
+      User Progress
+    </div>
+  )
+}
